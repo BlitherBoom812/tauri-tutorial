31ca63aaa846f75ab89bcb3490d45a9cb46b3e40
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2023-12-05 17:05:12 +0800

    add support for download

--- my_tauri/my-tauri/src-tauri/src/main.rs
+++ my_tauri/my-tauri/src-tauri/src/main.rs
@@ -2,18 +2,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
 
-#[tauri::command]
-async fn my_read_file(path: std::path::PathBuf) -> String {
-    // print
-    println!("{:?}", path);
-    // 读取文件内容，以文本字符串形式返回
-    std::fs::read_to_string(path).unwrap()
-}
+// #[tauri::command]
+// async fn my_read_file(path: std::path::PathBuf) -> String {
+//     // print
+//     println!("{:?}", path);
+//     // 读取文件内容，以文本字符串形式返回
+//     std::fs::read_to_string(path).unwrap()
+// }
 
 use std::collections::HashMap;
 
@@ -21,12 +21,14 @@ use std::collections::HashMap;
 fn handle_action(name: &str){
     
     let mut hw_dir_dict = HashMap::new();
+    hw_dir_dict.insert("下载", HashMap::from([("program", ["explorer.exe"]), ("args", ["C:\\Users\\Guo_Yun\\Downloads"])]));
     hw_dir_dict.insert("量筒", HashMap::from([("args", ["D:\\STUDY\\课程资料\\量筒\\hw"]), ("program", ["explorer.exe"])]));
     hw_dir_dict.insert("通网", HashMap::from([("args", ["D:\\STUDY\\课程资料\\通网\\hw"]), ("program", ["explorer.exe"])]));
     hw_dir_dict.insert("随机过程", HashMap::from([("args", ["D:\\STUDY\\课程资料\\随机过程\\hw"]), ("program", ["explorer.exe"])]));
     hw_dir_dict.insert("DSP", HashMap::from([("args", ["D:\\STUDY\\课程资料\\DSP"]), ("program", ["explorer.exe"])]));
     hw_dir_dict.insert("计网", HashMap::from([("args", ["D:\\STUDY\\课程资料\\计网\\hw"]), ("program", ["explorer.exe"])]));
     hw_dir_dict.insert("启动", HashMap::from([("program", ["C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"]), ("args", ["https://www.bilibili.com/video/BV1a14y1i7N4/"])]));
+    
 
     // print
     println!("opening: {:?}", name);
@@ -35,12 +37,26 @@ fn handle_action(name: &str){
     cmd.args(hw_dir_dict.get(name).unwrap().get("args").unwrap());
     cmd.spawn().unwrap();
     
-    
+
 }
 
 
 mod setup;
 mod tray;
+
+use windows_hotkeys::keys::{ModKey, VKey};
+use windows_hotkeys::{HotkeyManager, HotkeyManagerImpl};
+
+fn set_hotkey() {
+    let mut hkm = HotkeyManager::new();
+
+    hkm.register(VKey::A, &[ModKey::Ctrl], || {
+        println!("Hotkey ALT + A was pressed");
+    })
+    .unwrap();
+
+    hkm.event_loop();
+}
 
 fn main() {
     let context = tauri::generate_context!();
@@ -49,8 +65,10 @@ fn main() {
         .system_tray(tray::menu()) // ✅ 将 `tauri.conf.json` 上配置的图标添加到系统托盘
         .on_system_tray_event(tray::handler) // 添加系统托盘的 handler
         .setup(setup::init)
-        .invoke_handler(tauri::generate_handler![greet, my_read_file, handle_action]) // 添加传统 handler
+        .invoke_handler(tauri::generate_handler![handle_action]) // 添加传统 handler
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+    set_hotkey();
 }
 
--- my_tauri/my-tauri/src/App.tsx
+++ my_tauri/my-tauri/src/App.tsx
@@ -1,13 +1,8 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
-import React from 'react';
 import { Button, Flex } from 'antd';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
 
 
 
@@ -18,12 +13,13 @@ function App() {
 
   // var def
   const button_list = [
+    "下载",
     "量筒",
     "通网",
     '随机过程',
     "DSP",
     '计网',
-    '启动'
+    '启动',
   ]
 
   async function open_file(name: string) {
@@ -33,45 +29,10 @@ function App() {
   }
   
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+    
+      {/* now, it is the time to build antd components */}
       <Flex vertical={false} align="center" justify="center">
         {button_list.map((name) => (
           <Button type="primary" onClick={() => open_file(name)} shape="round">
@@ -85,3 +46,9 @@ function App() {
 }
 
 export default App;
+
+// 下一步
+// 模块化
+// 添加快捷键
+// 变成虚拟桌面
+// 自动拉取最新的 homework，课件，ppt
