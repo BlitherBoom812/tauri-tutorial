befb562fda4aa5f3108827521a7f0d996c56d70d
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2023-12-30 22:16:09 +0800

    add sodify url

--- my_tauri/my-tauri/src/App.tsx
+++ my_tauri/my-tauri/src/App.tsx
@@ -38,28 +38,3 @@ export default App;
 // // 变成虚拟桌面
 // // 自动拉取最新的 homework，课件，ppt
 // // 添加折叠菜单（ok)
-
-// import Draggable from 'react-draggable';
-// import './App.css';
-
-// const Box = () => {
-//   return (
-//     <div className="box">
-//       <div className="drag-handler">拖这里可以</div>
-//       <div>box 正文，拖这里拖不动</div>
-//     </div>
-//   );
-// };
-// function App() {
-//   return (
-//     <div className="App">
-//       <Draggable>
-//       <div>
-//         <Box></Box>
-//       </div>
-//       </Draggable>
-//     </div>
-//   );
-// }
-
-// export default App;
--- my_tauri/my-tauri/src/components/webButton.tsx
+++ my_tauri/my-tauri/src/components/webButton.tsx
@@ -1,24 +1,75 @@
-import { Button } from 'antd';
+import { CodeOutlined, CopyOutlined, MonitorOutlined } from '@ant-design/icons';
+import { Button, Flex, Input, Tooltip, message } from 'antd';
 import axios from 'axios';
+import { useState } from 'react';
 
 const WebButton: React.FC = (props: any) => {
-    const onClick = () => {
-        console.log(props.match.params.action)
-        axios
-        .post('http://localhost:12345/conv/button', {
-          action: props.match.params.action
-        })
-        .then(function (response) {
-          console.log(response.data.message);
-        //   setResultText(response.data.message);
-        })
-        .catch(function (error) {
-          console.log(error);
-        //   message(error.message);
-        });
-    }
-    console.log(props)
-  return <Button onClick={onClick}>{props.match.params.name}</Button>;
+  const [editing, setEditing] = useState(false);
+  const decoded_action = decodeURIComponent(props.match.params.action);
+  const [action, setAction] = useState(decoded_action);
+  const onActionClick = () => {
+    console.log('acting: ', action);
+    axios
+      .post('http://localhost:12345/conv/button', {
+        action: action,
+      })
+      .then(function (response) {
+        console.log(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  console.log(props);
+
+  const onEditClick = () => {
+    setEditing(!editing);
+    console.log('edit');
+  };
+
+  const onEditChange = (val: any) => {
+    console.log(val.target.value);
+    setAction(val.target.value);
+    props.match.params.action = val.target.value;
+  };
+  const onCopyClick = () => {
+    console.log('copy');
+    // copy current url
+    navigator.clipboard.writeText(window.location.origin + '/button/' + props.match.params.name +  '/' + encodeURIComponent(action));
+    message.success('Copied!');
+  };
+
+  return (
+    <div>
+      <Flex vertical={false}>
+        {editing ? (
+          <Flex vertical={false} style={{ width: '100%' }}>
+            <Input
+              prefix={<CodeOutlined className="site-form-item-icon" />}
+              onChange={onEditChange}
+              defaultValue={action}
+            ></Input>
+            <Tooltip title="Copy URL">
+              <Button
+                shape="circle"
+                icon={<CopyOutlined />}
+                onClick={onCopyClick}
+              ></Button>
+            </Tooltip>
+          </Flex>
+        ) : (
+          <Button onClick={onActionClick} style={{ width: '100%' }}>
+            {props.match.params.name}
+          </Button>
+        )}
+        <Button
+          shape="circle"
+          icon={<MonitorOutlined />}
+          onClick={onEditClick}
+        ></Button>
+      </Flex>
+    </div>
+  );
 };
 
 export { WebButton };
