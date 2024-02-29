b0b483d14d84c64ab36fdc29adcd3d4288676693
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2024-02-29 18:45:23 +0800

    add author

--- my_tauri/my-tauri/src/App.tsx
+++ my_tauri/my-tauri/src/App.tsx
@@ -1,12 +1,28 @@
 import './App.css';
+import Author from './components/author';
 
 import SearchBox from './components/searchbar';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Author/>
+  },
+  {
+    path: "/searchbox",
+    element: <SearchBox></SearchBox>
+  },
+]);
 
 function App() {
   return (
     <div className="container">
       {/* now, it is the time to build antd components */}
-      <SearchBox></SearchBox>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ my_tauri/my-tauri/src/components/author.tsx
@@ -0,0 +1,57 @@
+import '../styles/shining_txt.css';
+import { PhysicalSize, WebviewWindow } from '@tauri-apps/api/window';
+
+function createWindow() {
+  const webview = new WebviewWindow('searchbox', {
+    url: 'searchbox',
+    fullscreen: false,
+    resizable: false,
+    visible: true,
+    title: 'searchbox',
+    alwaysOnTop: true,
+    decorations: false,
+    transparent: true,
+    width: 1000,
+    height: 100,
+    center: true
+  });
+  // since the webview window is created asynchronously,
+  // Tauri emits the `tauri://created` and `tauri://error` to notify you of the creation response
+  webview.once('tauri://created', function () {
+    // webview window successfully created
+    console.log('window created!');
+  });
+  webview.once('tauri://error', function (e: any) {
+    // an error occurred during webview window creation
+    console.log(`error creating window:`);
+    // convert e to string
+    console.log(e);
+  });
+  return webview;
+}
+const webview = createWindow();
+
+function Author() {
+
+  const text = 'Made by BlitherBoom812';
+  // visit every char for text
+  const array = text.split(' ');
+  
+  return (
+    <div
+      onClick={async () => {
+        if (await webview.isVisible()) {
+          webview.hide();
+        } else {
+          webview.show();
+        }
+      }}
+    >
+      {array.map((item, _) => (
+        <span>{item} </span>
+      ))}
+    </div>
+  );
+}
+
+export default Author;
