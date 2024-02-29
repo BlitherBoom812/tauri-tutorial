50ba8616275d969fe2c22b1da8edf76edbb2541f
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2024-02-29 18:06:12 +0800

    add multi window

--- my_tauri/my-tauri/src/App.tsx
+++ my_tauri/my-tauri/src/App.tsx
@@ -1,8 +1,5 @@
 import './App.css';
-import { Flex, Space } from 'antd';
-import { CurriculumBar } from './components/curriculum';
-import { CurriculumList } from './data/user_data';
-import Draggable from 'react-draggable';
+
 import SearchBox from './components/searchbar';
 
 function App() {
@@ -15,35 +12,3 @@ function App() {
 }
 
 export default App;
-
-// // 下一步
-// // 模块化
-// // 添加快捷键
-// // 变成虚拟桌面
-// // 自动拉取最新的 homework，课件，ppt
-// // 添加折叠菜单（ok)
-
-// import Draggable from 'react-draggable';
-// import './App.css';
-
-// const Box = () => {
-//   return (
-//     <div className="box">
-//       <div className="drag-handler">拖这里可以</div>
-//       <div>box 正文，拖这里拖不动</div>
-//     </div>
-//   );
-// };
-// function App() {
-//   return (
-//     <div className="App">
-//       <Draggable>
-//       <div>
-//         <Box></Box>
-//       </div>
-//       </Draggable>
-//     </div>
-//   );
-// }
-
-// export default App;
--- my_tauri/my-tauri/src/main.tsx
+++ my_tauri/my-tauri/src/main.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import { PhysicalSize, WebviewWindow } from '@tauri-apps/api/window';
+
+const webview = new WebviewWindow('theUniqueLabel', {
+  url: 'index.html',
+  "fullscreen": false,
+  "resizable": false,
+  "visible": true,
+  "title": "SpellMagic",
+  "alwaysOnTop": true,
+  "decorations": false,
+  "transparent": true,
+  "width": 800,
+  "height": 500
+});
+// since the webview window is created asynchronously,
+// Tauri emits the `tauri://created` and `tauri://error` to notify you of the creation response
+webview.once('tauri://created', function () {
+  // webview window successfully created
+  console.log('window created!');
+});
+webview.once('tauri://error', function (e: any) {
+  // an error occurred during webview window creation
+  console.log(`error creating window:`);
+  // convert e to string
+  console.log(e);
+});
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
